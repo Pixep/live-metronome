c38e4cb305dd27ee3175d06d0da5e6b8fce16839
Adrien Leravat <adrien.leravat@gmail.com>
2016-11-26 15:00:23 +0100

    Updated MSV project make file

--- makeVsProject.bat
+++ makeVsProject.bat
@@ -1,1 +1,6 @@
-E:\Dev\Qt\5.7\winrt_armv7_msvc2015\bin\qmake -tp vc Metronome.pro
+IF EXIST E:\Dev\Qt\5.7\winrt_armv7_msvc2015\bin\qmake (
+	E:\Dev\Qt\5.7\winrt_armv7_msvc2015\bin\qmake -tp vc Metronome.pro
+) ELSE (
+	REM -spec win32-msvc2015
+	C:\Qt\5.7\winrt_armv7_msvc2015\bin\qmake -tp vc -r Metronome.pro
+)

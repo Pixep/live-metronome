bf54c69064586a24795d297c3e3c977e588827aa
Adrien Leravat <adrien.leravat@gmail.com>
2016-11-26 16:39:16 +0100

    Updated french translations

--- translations/fr-FR.ts
+++ translations/fr-FR.ts
@@ -4,22 +4,22 @@
 <context>
     <name>AddEditSongPage</name>
     <message>
-        <location filename="../qml/pages/AddEditSongPage.qml" line="90"/>
+        <location filename="../qml/pages/AddEditSongPage.qml" line="129"/>
         <source>Title</source>
         <translation>Titre</translation>
     </message>
     <message>
-        <location filename="../qml/pages/AddEditSongPage.qml" line="106"/>
+        <location filename="../qml/pages/AddEditSongPage.qml" line="145"/>
         <source>Artist</source>
         <translation>Artiste</translation>
     </message>
     <message>
-        <location filename="../qml/pages/AddEditSongPage.qml" line="117"/>
+        <location filename="../qml/pages/AddEditSongPage.qml" line="156"/>
         <source>Tempo</source>
         <translation>Tempo</translation>
     </message>
     <message>
-        <location filename="../qml/pages/AddEditSongPage.qml" line="148"/>
+        <location filename="../qml/pages/AddEditSongPage.qml" line="187"/>
         <source>Beats per measure</source>
         <translation>Temps par mesure</translation>
     </message>
@@ -27,12 +27,17 @@
 <context>
     <name>ApplicationHeader</name>
     <message>
-        <location filename="../qml/components/ApplicationHeader.qml" line="38"/>
-        <source>Live Metronome</source>
+        <location filename="../qml/components/ApplicationHeader.qml" line="61"/>
+        <source>Setlists</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../qml/components/ApplicationHeader.qml" line="57"/>
+        <location filename="../qml/components/ApplicationHeader.qml" line="74"/>
+        <source>Setlist name</source>
+        <translation>Nom de la setlist</translation>
+    </message>
+    <message>
+        <location filename="../qml/components/ApplicationHeader.qml" line="104"/>
         <source>Menu disabled during play</source>
         <translation>Menu désactivé pendant la lecture</translation>
     </message>
@@ -40,20 +45,92 @@
 <context>
     <name>ConfirmDialog</name>
     <message>
-        <location filename="../qml/dialogs/ConfirmDialog.qml" line="80"/>
         <source>Cancel</source>
-        <translation>Annuler</translation>
+        <translation type="vanished">Annuler</translation>
     </message>
     <message>
-        <location filename="../qml/dialogs/ConfirmDialog.qml" line="89"/>
         <source>Ok</source>
-        <translation>Valider</translation>
+        <translation type="vanished">Valider</translation>
+    </message>
+</context>
+<context>
+    <name>MainMenu</name>
+    <message>
+        <location filename="../qml/MainMenu.qml" line="23"/>
+        <source>Songs</source>
+        <translation>Musiques</translation>
+    </message>
+    <message>
+        <location filename="../qml/MainMenu.qml" line="23"/>
+        <location filename="../qml/MainMenu.qml" line="41"/>
+        <source>Setlists</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/MainMenu.qml" line="52"/>
+        <source>New setlist</source>
+        <translation>Nouvelle setlist</translation>
+    </message>
+    <message>
+        <location filename="../qml/MainMenu.qml" line="63"/>
+        <source>Rename setlist</source>
+        <translation>Renommer setlist</translation>
+    </message>
+    <message>
+        <location filename="../qml/MainMenu.qml" line="75"/>
+        <source>Delete setlist</source>
+        <translation>Suppr. une setlist</translation>
+    </message>
+    <message>
+        <location filename="../qml/MainMenu.qml" line="98"/>
+        <source>Edit current song</source>
+        <translation>Editer la musique</translation>
+    </message>
+    <message>
+        <location filename="../qml/MainMenu.qml" line="121"/>
+        <source>Add new song</source>
+        <translation>Ajouter une musique</translation>
+    </message>
+    <message>
+        <location filename="../qml/MainMenu.qml" line="131"/>
+        <source>Change songs order</source>
+        <translation>Modifier l&apos;ordre</translation>
+    </message>
+    <message>
+        <location filename="../qml/MainMenu.qml" line="142"/>
+        <source>Clear all</source>
+        <translation>Tout effacer</translation>
+    </message>
+    <message>
+        <location filename="../qml/MainMenu.qml" line="147"/>
+        <source>Do you confirm removing all songs from the set ?</source>
+        <translation>Confirmez-vous la suppression de toutes les musiques de cette setliste ?</translation>
+    </message>
+    <message>
+        <location filename="../qml/MainMenu.qml" line="161"/>
+        <source>Reset all</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/MainMenu.qml" line="164"/>
+        <source>Do you confirm resetting all set content ?</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/MainMenu.qml" line="177"/>
+        <source>Settings</source>
+        <translation>Réglages</translation>
+    </message>
+    <message>
+        <location filename="../qml/MainMenu.qml" line="186"/>
+        <source>Cancel</source>
+        <translation>Annuler</translation>
     </message>
 </context>
 <context>
     <name>MainPage</name>
     <message>
-        <location filename="../qml/pages/MainPage.qml" line="85"/>
+        <location filename="../qml/pages/MainPage.qml" line="95"/>
         <source>Do you confirm removing &apos;%1&apos; ?</source>
         <translation>Est-ce que vous confirmez la suppression de &apos;%1&apos; ?</translation>
     </message>
@@ -61,50 +138,34 @@
 <context>
     <name>MenuDialog</name>
     <message>
-        <location filename="../qml/MenuDialog.qml" line="10"/>
         <source>Edit song</source>
-        <translation>Modifier la musique</translation>
+        <translation type="vanished">Modifier la musique</translation>
     </message>
     <message>
-        <location filename="../qml/MenuDialog.qml" line="18"/>
         <source>Add new song</source>
-        <translation>Ajouter une musique</translation>
+        <translation type="vanished">Ajouter une musique</translation>
     </message>
     <message>
-        <location filename="../qml/MenuDialog.qml" line="29"/>
         <source>Change songs order</source>
-        <translation>Modifier l&apos;ordre</translation>
+        <translation type="vanished">Modifier l&apos;ordre</translation>
     </message>
     <message>
-        <location filename="../qml/MenuDialog.qml" line="38"/>
         <source>Clear all</source>
-        <translation>Tout effacer</translation>
+        <translation type="vanished">Tout effacer</translation>
     </message>
     <message>
-        <location filename="../qml/MenuDialog.qml" line="41"/>
         <source>Do you confirm removing all songs from the set ?</source>
-        <translation>Confirmez-vous la suppression de toutes les musiques de cette liste ?</translation>
+        <translation type="vanished">Confirmez-vous la suppression de toutes les musiques de cette liste ?</translation>
     </message>
     <message>
-        <location filename="../qml/MenuDialog.qml" line="57"/>
-        <source>Reset all</source>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="../qml/MenuDialog.qml" line="60"/>
-        <source>Do you confirm resetting all set content ?</source>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="../qml/MenuDialog.qml" line="75"/>
         <source>Cancel</source>
-        <translation>Annuler</translation>
+        <translation type="vanished">Annuler</translation>
     </message>
 </context>
 <context>
     <name>MoveSongsPage</name>
     <message>
-        <location filename="../qml/pages/MoveSongsPage.qml" line="58"/>
+        <location filename="../qml/pages/MoveSongsPage.qml" line="91"/>
         <source>Discard your modifications ?</source>
         <translation>Annuler vos modifications ?</translation>
     </message>
@@ -118,6 +179,85 @@
     <message>
         <source>Save</source>
         <translation type="vanished">Sauver</translation>
+    </message>
+</context>
+<context>
+    <name>Setlist</name>
+    <message>
+        <location filename="../setlist.cpp" line="4"/>
+        <source>New setlist</source>
+        <translation>Nouvelle setlist</translation>
+    </message>
+</context>
+<context>
+    <name>SetlistActionDialog</name>
+    <message>
+        <location filename="../qml/pages/SetlistPage/SetlistActionDialog.qml" line="12"/>
+        <source>Rename</source>
+        <translation>Renommer</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SetlistPage/SetlistActionDialog.qml" line="19"/>
+        <source>Delete</source>
+        <translation>Supprimer</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SetlistPage/SetlistActionDialog.qml" line="26"/>
+        <source>Cancel</source>
+        <translation>Annuler</translation>
+    </message>
+</context>
+<context>
+    <name>SetlistListView</name>
+    <message>
+        <location filename="../qml/pages/SetlistPage/SetlistListView.qml" line="23"/>
+        <source>No song present your set&lt;br&gt;Add one with the %1 button</source>
+        <translation>Aucune musique présente&lt;br&gt;Utilisez %1 pour en ajouter une</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SetlistPage/SetlistListView.qml" line="38"/>
+        <source>emtpy</source>
+        <translation>vide</translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../qml/pages/SetlistPage/SetlistListView.qml" line="38"/>
+        <source>%n song(s)</source>
+        <translation>
+            <numerusform>%n musique</numerusform>
+            <numerusform>%n musiques</numerusform>
+        </translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SetlistPage/SetlistListView.qml" line="50"/>
+        <source>Add setlist</source>
+        <translation>Ajouter une setlist</translation>
+    </message>
+</context>
+<context>
+    <name>SetlistPage</name>
+    <message>
+        <location filename="../qml/pages/SetlistPage.qml" line="16"/>
+        <location filename="../qml/pages/SetlistPage.qml" line="60"/>
+        <source>Setlist name</source>
+        <translation>Nom de la setlist</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SetlistPage.qml" line="65"/>
+        <source>Do you confirm completly removing the setlist &apos;%1&apos; ?</source>
+        <translation>Confirmez-vous la suppression de la setlist &apos;%1&apos; ?</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SetlistPage.qml" line="133"/>
+        <source>current</source>
+        <translation>courante</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="85"/>
+        <source>Language</source>
+        <translation>Langue</translation>
     </message>
 </context>
 <context>
@@ -175,6 +315,16 @@
         <location filename="../qml/controls/TempoControls.qml" line="76"/>
         <source>Tap tempo</source>
         <translation></translation>
+    </message>
+</context>
+<context>
+    <name>UserSettings</name>
+    <message>
+        <location filename="../usersettings.cpp" line="100"/>
+        <location filename="../usersettings.cpp" line="185"/>
+        <location filename="../usersettings.cpp" line="316"/>
+        <source>New setlist</source>
+        <translation>Nouvelle setlist</translation>
     </message>
 </context>
 </TS>

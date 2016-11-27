07789b617cf5f39c4d5e29c920f8cf87f0e650df
Adrien Leravat <adrien.leravat@gmail.com>
2016-11-27 14:18:13 +0100

    Updated make vs batch to fix a "exist" problem

--- makeVsProject.bat
+++ makeVsProject.bat
@@ -1,6 +1,8 @@
-IF EXIST E:\Dev\Qt\5.7\winrt_armv7_msvc2015\bin\qmake (
-	E:\Dev\Qt\5.7\winrt_armv7_msvc2015\bin\qmake -tp vc Metronome.pro
+@ECHO OFF
+IF EXIST E:\Dev\Qt\5.7\winrt_armv7_msvc2015\bin (
+	ECHO "Building from E:\Dev\Qt"
+	E:\Dev\Qt\5.7\winrt_armv7_msvc2015\bin\qmake -tp vc -r Metronome.pro
 ) ELSE (
-	REM -spec win32-msvc2015
+	ECHO "Building from C:\Qt"
 	C:\Qt\5.7\winrt_armv7_msvc2015\bin\qmake -tp vc -r Metronome.pro
 )
